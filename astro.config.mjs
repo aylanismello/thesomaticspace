811da8b10649e68d00384d067b918c69861fb5f0
Aylan Mello <aylanismello@gmail.com>
2025-12-29 18:41:31 +0800

    update for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: netlify(),
-  vite: {
-    plugins: [tailwindcss()],
-  },
   site: 'https://thesomaticspace.com',
 });
